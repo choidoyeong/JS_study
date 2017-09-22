af57f0116994a0974444229095c6c1d63762e62f
choidoyeong <omok5674@naver.com>
2017-09-22 23:45:48 +0900

    2017.9.22*

--- doit_node.js_Express/app7.js
+++ doit_node.js_Express/app7.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // body-parser를 사용해 application/json 파싱
 app.use(bodyParser.json());
 
-app.use(static(path.join(__dirname, 'public')));
+app.use('/public', static(path.join(__dirname, 'public')));
 
 // 미들웨어에서 파라미터 확인
 app.use(function(req, res, next) {
@@ -28,18 +28,11 @@ app.use(function(req, res, next) {
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
     
-    res.writeHead('200', {'Content-type':'text/html;charset=uft8'});
+    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
     res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
     res.write('<div><p>Param id : '+ paramId + '</p></div>');
     res.write('<div><p>Param password : '+ paramPassword + '</p></div>');
     res.end();
-});
-
-app.use('/', function(req, res, next) {
-    console.log('두 번째 미들웨어에서 요청을 처리함.');
-    
-    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-    res.end('<h1>Express 서버에서 ' + req.user + '가 응답한 결과입니다.</h1>');
 });
 
 http.createServer(app).listen(app.get('port'), function(){
